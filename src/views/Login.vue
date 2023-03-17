9665cf77526fd1ce7a7d58a06cacfd4ceaa7308d
Ameysingh Bayas <ameybayas@gmail.com>
2023-03-17 12:21:37 +0530

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="center">
+        <v-row>
+            <v-col cols="10" sm="6" offset-sm="3" md="4" offset-md="4" offset="1">
+                <v-avatar size="100" tile>
+                    <v-img src="@/assets/thelogo.png"></v-img>
+                </v-avatar>
+
+                <v-text-field label="Username" v-model="username"></v-text-field>
+                <v-text-field label="Password" v-model="password" type="password"></v-text-field>
+                <v-btn type="submit" @click="auth()">
+                    Login
+                </v-btn>
+
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        username = "";
+        password = "";
+    },
+    methods: {
+        auth() {
+            if (this.username == "ameybayas@gmail.com" && this.password == "pwd") {
+                alert("Welcome Amey")
+            }
+            else {
+                alert("Wrong Password")
+            }
+        }
+    },
+
+}
+</script>
+
+<style lang="scss" scoped></style>
